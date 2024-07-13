f7d347a145a676f4751ad2e0c8c7987d5c2850cf
carlospaterninadev <ingcarlospaternina@gmail.com>
2024-07-12 20:55:28 -0500

    feat: developer page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import Developer from "./pages/Developer"
 
 const App = (props) => {
 
@@ -11,6 +12,7 @@ const App = (props) => {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/developer" element={<Developer />} />
         <Route path="/*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
