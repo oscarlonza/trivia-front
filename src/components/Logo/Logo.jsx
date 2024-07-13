2b546256c6b0d51ed6f4af92f6c27159fbf5c0fb
jeguzman <guzmanmercado.je@gmail.com>
2024-07-12 21:31:38 -0500

    Grupo 8, la final

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,21 +1,13 @@
 import { Link } from "react-router-dom"
-<<<<<<< HEAD
-
-=======
 import style from './logo.module.css'
 
 const {img} = style
->>>>>>> d01a87069dc8c80abcba971c474fd2d0827de5c5
 
 const Logo = () => {
     return (
         <Link to={'/'}>
             <picture>
-<<<<<<< HEAD
-                <img src="./Logo.png" alt="Logo trivia" />
-=======
                 <img className={img} src="./Logo.png" alt="Logo trivia" />
->>>>>>> d01a87069dc8c80abcba971c474fd2d0827de5c5
             </picture>
         </Link>
     )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,16 +7,6 @@ import style from './login.module.css'
 
 const Login = () => {
 
-<<<<<<< HEAD
-  const { card, logo } = style
-
-  return (
-    <div className={card}>
-      <div className={logo}>
-          <Logo />
-      </div>
-      <div>
-=======
   const { fondo, card} = style
 
   return (
@@ -24,7 +14,6 @@ const Login = () => {
     <div className={fondo}>
       <div className={card}>        
           <Logo />          
->>>>>>> d01a87069dc8c80abcba971c474fd2d0827de5c5
           <LoginForm />
       </div>
       <div>
@@ -33,14 +22,10 @@ const Login = () => {
       <div>
         <Alert />
       </div>
-<<<<<<< HEAD
-    </div>
-=======
 
     </div>
       
    
->>>>>>> d01a87069dc8c80abcba971c474fd2d0827de5c5
   )
 }
 
