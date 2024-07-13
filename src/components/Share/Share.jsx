a0f0a3c493f17a3b148fa5d8c3937c162cd54e79
gmmonsalve <gbrielamonsalve@gmail.com>
2024-07-12 21:43:52 -0500

    Refactor modal

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -1,49 +1,33 @@
 import style from './Share.module.css'
+import Modal from '../Modal';
+import React from "react";
 
 const Share = () => {
+    const [open, setOpen] = React.useState(false);
 
-    const openShareModal = ()=>{
-        console.log('a')
-       
-            var modal = document.getElementById("shareModal");
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-            // Get the button that opens the modal
-            var btn = document.getElementById("shareButton");
-    
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-    
-            // When the user clicks on the button, open the modal
-            btn.onclick = function () {
-                modal.style.display = "block";
-            }
-    
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function () {
-                modal.style.display = "none";
-            }
-    
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function (event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
-                }
-            }
-        
-    }
+    const handleOpen = () => {
+        setOpen(true);
+    };
     return (
         <>
-            <button id={style.shareButton} onClick={openShareModal}>
+            <button id={style.shareButton} onClick={handleOpen}>
                 <img id={style.shareIcon} src="./share-icon.png" alt="Logo trivia" />
             </button>
 
-            <div id={style.shareModal}>
-                <div className={style.modalContent}>
-                    <span className={style.close}>&times;</span>
-                    <p>Ingresa el número para invitar a un amigo a jugar trivia Marvel!</p>
-                    <input type="tel" placeholder='+57 xxx xxxxxxx'></input>
-                </div>
-            </div>
+            <Modal isOpen={open} onClose={handleClose}>
+                <>
+                    <h1>Compartir</h1>
+                    <p>Invita a tus amigos a jugar trivia Marvel a través de SMS!</p>
+                    <form>
+                        <input type="tel" placeholder="+57 xxx xxxxxxx"></input>
+                        <button type='submit'>Enviar</button>
+                    </form>
+                </>
+            </Modal>
         </>
 
     )
